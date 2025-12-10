88798828946e6141729f427345afeebdfba469c3
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-12-10 23:50:54 +0600

    route change

--- Analytics/analytics.controller.js
+++ Analytics/analytics.controller.js
@@ -3,7 +3,26 @@ function track(id) {
   const body = document.getElementById(id);
   addEventListener('click', (e) => {
     // console.log(e.target.tagName);
-    console.log('clicked :' + e.target.innerText);
+    // console.log('clicked :' + e.target.innerText);
+    if (e.target.tagName == 'BUTTON') {
+      console.log('button clicked: ' + e.target.innerText);
+      console.log('at: ' + window.location.pathname);
+    }
   });
+  function fireRouteChange(route) {
+    console.log('route changed');
+    console.log('at: ' + route);
+  }
+  function trackRoute() {
+    ['pushState', 'replaceState'].forEach((method) => {
+      const original = history[method];
+      history[method] = function () {
+        fireRouteChange(window.location.pathname);
+        return original.apply(this, arguments);
+      };
+    });
+  }
+
+  trackRoute();
 }
 export default { track };
